978b6b3ceb59386cdbc2604d3d379374ae3b8917
Renen Maharaj <someshrenen@gmail.com>
2025-05-25 03:06:57 +0200

    added front/backend environments

---
+++ backend/src/test/java/com/rs_budget_app/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rs_budget_app.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
